fbfe0f92ea3255c1fd7441a533c95c4cc68a2e47
marcusenzotlising <152583265+marcusenzotlising@users.noreply.github.com>
2024-03-02 11:57:41 +0800

    First Commit
    03-02-2024

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What year did the first Star Wars movie come out??",
+    answer: "1977",
+    options: [
+      "1977",
+      "1978",
+      "1979",
+      "1980"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What's the name of Boba Fett's ship?",
+    answer: "Slave 1",
+    options: [
+      "Millenium Falcon",
+      "Razor Crest",
+      "Slave 1",
+      "X Wing"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the episode number of the very first Star Wars film?",
+    answer: "IV",
+    options: [
+      "I",
+      "II",
+      "III",
+      "IV"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Chancellor Palpatine was which Sith Lord?",
+    answer: "Darth Sidious",
+    options: [
+      "Darth Maul",
+      "Darth Sidious",
+      "Darth Vader",
+      "Darth Bane"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who killed Jabba the Hutt?",
+    answer: "Princess Leia",
+    options: [
+      "Luke Skywalker",
+      "Hans Solo",
+      "Princess Leia",
+      "Master Yoda"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
